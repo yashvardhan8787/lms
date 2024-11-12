29dfa13269d35639c1112eb09673e890a03a14ba
yashvardhan8787 <139737436+yashvardhan8787@users.noreply.github.com>
2024-11-12 11:45:25 +0530

    mm

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -95,7 +95,11 @@ const router = express.Router();
 
 // Course Routes
 router.post('/create',
+<<<<<<< Updated upstream
   //isAutheticated,authorizeRoles('admin'), 
+=======
+  // isAutheticated,authorizeRoles('admin'), 
+>>>>>>> Stashed changes
   createCourse);               // Create a new course
 router.put('/update/:id', 
   // isAutheticated,
